5ec701d30a2f8064ed32f9be1f07add5e58a9952
nazarporada <103384201+nazarporada@users.noreply.github.com>
2023-02-04 12:56:02 +0200

    Add files via upload

--- 123.py
+++ 123.py
@@ -1,12 +1,10 @@
-import webbrowser
-from datetime import datetime
+import requests
 import os
-from gtts import gTTS
+from datetime import datetime
+
 import pygame
-import datetime
-
 import speech_recognition as sr
-
+from gtts import gTTS
 
 
 def talk(words):
@@ -45,6 +43,7 @@ def command():
 
 
 def makeSomething(zadanie):
+    global data
     if 'привіт' in zadanie:
 
         tts2 = gTTS("дай Боже", lang='uk')
@@ -78,8 +77,36 @@ def makeSomething(zadanie):
         pygame.mixer.music.load('output6.mp3')
         pygame.mixer.music.play()
     elif 'яка погода' in zadanie:
-        webbrowser.open("https://ua.sinoptik.ua/погода-львів")
-        tts7 = gTTS("", lang='uk')
+
+        s_city = "Lviv, UA"
+        city_id = 702550
+        appid = "d972ec87c4d3b913ea5d14a1a6a9ac34"
+        try:
+            res = requests.get("http://api.openweathermap.org/data/2.5/find",
+                               params={'q': s_city, 'type': 'like', 'units': 'metric', 'APPID': appid})
+            data = res.json()
+            cities = ["{} ({})".format(d['name'], d['sys']['country'])
+                      for d in data['list']]
+            print("city:", cities)
+            city_id = data['list'][0]['id']
+            print('city_id=', city_id)
+        except Exception as e:
+            print("Exception (find):", e)
+            pass
+
+        try:
+            res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+                               params={'id': city_id, 'units': 'metric', 'lang': 'uk', 'APPID': appid})
+            data = res.json()
+            print("conditions:", data['weather'][0]['description'])
+            print("temp:", data['main']['temp'])
+            print("temp_min:", data['main']['temp_min'])
+            print("temp_max:", data['main']['temp_max'])
+        except Exception as e:
+            print("Exception (weather):", e)
+            pass
+
+        tts7 = gTTS(data['weather'][0]['description'] + str(data['main']['temp']), lang='uk')
         tts7.save('output7.mp3')
         pygame.mixer.init()
         pygame.mixer.music.load('output7.mp3')
